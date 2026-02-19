39f12d12bc6d2451db143d679dfa34a7f61ae55b
Shebin <shebin@interlandtech.com>
2026-02-19 11:40:00 +0530

    localhost port changed to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,12 @@
 export const environment = {
     production:true,
-    loginApi:'http://localhost:1500/api/auth',
-    api:'http://localhost:1500/api/user'
+    // loginApi:'http://localhost:1500/api/auth',
+    // api:'http://localhost:1500/api/user'
     // loginApi: 'http://host.docker.internal:1500/api/auth',
     // api: 'http://host.docker.internal:1500/api/user'
+
+    // For production
+     loginApi:' https://nonproducing-contumeliously-kane.ngrok-free.dev/api/auth',
+    api:' https://nonproducing-contumeliously-kane.ngrok-free.dev/api/user'
 };
 
